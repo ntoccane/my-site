b4cda71d1207303af9719efff79ed5b717428bb9
Niccolo Toccane <ntoccane@adobe.com>
2024-07-09 18:25:49 +0200

    fix filter experiment

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -16,6 +16,21 @@ export default function decorate(block) {
     ul.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
     block.textContent = '';
     block.append(ul);
+  } else if (document.body.classList.contains('variant-challenger-2')) {
+    /* change to ul, li */
+    const ul = document.createElement('ul');
+    [...block.children].forEach((row) => {
+      const li = document.createElement('li');
+      while (row.firstElementChild) li.append(row.firstElementChild);
+      [...li.children].forEach((div) => {
+        if (div.children.length === 1 && div.querySelector('picture')) div.className = 'variant-cards-card-image-challenger-2';
+        else div.className = 'cards-card-body';
+      });
+      ul.append(li);
+    });
+    ul.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
+    block.textContent = '';
+    block.append(ul);
   } else {
     /* change to ul, li */
     const ul = document.createElement('ul');
